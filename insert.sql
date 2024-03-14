dd01951b5f308ce158446987a1eae4d7118a6f5c
Theo45120 <theo.villain45000@gmail.com>
2024-03-14 15:27:13 +0100

    question 3.2 B me parait bon

--- insert.sql
+++ insert.sql
@@ -20,8 +20,10 @@ INSERT INTO Etape (CodeEtape, NomEtape, Latitude, Longitude, NiveauConfort) VALU
 (5, 'Sommet du Monte Cinto', 42.567890, 9.543210, 5),
 (6, 'Orléans', 47.902964, 1.909251, 1),
 (7, 'Blois', 47.5917, 1.3281, 2),
-(8, 'Tours', 47.3941, 0.6848, 3);
-
+(8, 'Tours', 47.3941, 0.6848, 3),
+(9, 'Châteaudun', 48.0815, 1.3361, 2),
+(10, 'Chartres', 48.4439, 1.4897, 2),
+(11, 'Paris', 42.1253, 1.6985, 1);
 
 -- Insertion des tronçons
 INSERT INTO Troncon (IdentifiantTroncon, NomUsuel, Distance, TypeSol, DeniveleMoyen, PointDepart, PointArrivee) VALUES
@@ -30,4 +32,7 @@ INSERT INTO Troncon (IdentifiantTroncon, NomUsuel, Distance, TypeSol, DeniveleMo
 (3, 'Sentier du Refuge du Col de Vergio à la Cascade des Anglais', 3.5, 'Terre', 150, 3, 4),
 (4, 'Sentier de la Cascade des Anglais au Sommet du Monte Cinto', 10.2, 'Terre', 800, 4, 5),
 (5, 'Sentier d''Orléans à Blois', 55.8, 'Bitume', 50, 6, 7),
-(6, 'Sentier d''Orléans à Tours', 122.5, 'Bitume', 80, 6, 8);
+(6, 'Sentier d''Orléans à Tours', 122.5, 'Bitume', 80, 6, 8),
+(7, 'Sentier d''Orléans à Châteaudun', 35.2, 'Bitume', 40, 6, 9),
+(8, 'Sentier de Châteaudun à Chartres', 45.6, 'Bitume', 30, 9, 10);
+(9, 'Sentier de Chartres à Paris', 180.2, 'Bitume', 20, 10, 11);
--- requete.sql
+++ requete.sql
@@ -1,6 +1,19 @@
--- QUESTION 3.2
+-- QUESTION 3.2 A
 SELECT DISTINCT e.NomEtape
 FROM Etape e
 INNER JOIN Troncon t ON e.CodeEtape = t.PointArrivee
 INNER JOIN Etape o ON t.PointDepart = o.CodeEtape
 WHERE o.NomEtape = 'Orléans';
+
+
+-- QUESTION 3.2 B
+SELECT e2.NomEtape AS Etape_Intermediaire, e3.NomEtape AS Etape_Arrivee
+FROM Etape e1
+JOIN Troncon t1 ON e1.CodeEtape = t1.PointDepart
+JOIN Etape e2 ON t1.PointArrivee = e2.CodeEtape
+JOIN Troncon t2 ON e2.CodeEtape = t2.PointDepart
+JOIN Etape e3 ON t2.PointArrivee = e3.CodeEtape
+WHERE e1.NomEtape = 'Orléans';
+
+
+-- QUESTION 3.2 C
